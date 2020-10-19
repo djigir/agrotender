de0cc96cfa7607c905b7c10e0afe17d9d0c053f8
Sydoroff <is.sydoroff@gmail.com>
2020-10-19 12:37:13 +0300

    getPageInfo

--- laravel/app/Services/SeoService.php
+++ laravel/app/Services/SeoService.php
@@ -3,11 +3,14 @@
 namespace App\Services;
 
 
+use App\Models\Pages\Pages;
+
 class SeoService
 {
-    public function getPageInfo()
+    public function getPageInfo($page)
     {
-
+        dd(Pages::where('page_name', $page)->get()->toArray());
+        return Pages::where('page_name', $page)->get()->toArray();
     }
 
     public function getBoardMeta()
