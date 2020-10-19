cf58738279dee08cc6bad43d1d2e97adf80bcefc
Sydoroff <is.sydoroff@gmail.com>
2020-10-19 12:04:19 +0300

    model and methods

---
+++ laravel/app/Services/SeoService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services;
+
+
+class SeoService
+{
+    public function getPageInfo()
+    {
+
+    }
+
+    public function getBoardMeta()
+    {
+
+    }
+    public function getCompaniesMeta()
+    {
+
+    }
+    public function getTradersMeta()
+    {
+
+    }
+    public function getAnaliticMeta()
+    {
+
+    }
+    public function parseSeoText()
+    {
+
+    }
+}
