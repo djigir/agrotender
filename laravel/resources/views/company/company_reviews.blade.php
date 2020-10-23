d9c8d2b1830f50a64cc9c9b964804bccbb88b862
Dmitry <djigirdds@gmail.com>
2020-10-23 10:12:47 +0300

    rewivs

--- laravel/resources/views/company/company_reviews.blade.php
+++ laravel/resources/views/company/company_reviews.blade.php
@@ -12,8 +12,10 @@
                 <h2 class="d-inline-block text-uppercase">Отзывы</h2>
             </div>
             <div class="col-8 text-center text-md-right">
-                <a href="#" class="top-btn btn btn-primary align-bottom addReview">
-                    <span class="pl-1 pr-1">Оставить отзыв</span>
+                <a href="#" class="top-btn btn btn-primary align-bottom"><!--addReview-->
+{{--                    <span class="pl-1 pr-1">Оставить отзыв</span>--}}
+<!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">Оставить отзыв</button>
                 </a>
             </div>
         </div>
@@ -39,7 +41,7 @@
                         <div class="col p-0">
 
                             @if(isset($review['title']))
-                                 <a href="{{ route('company.company', $review['comp_id']) }}" target="_blank">{{ $review['title'] }}</a>
+                                 <a href="{{ route('company.company', $review['comp_id']) }}" target="_blank">{!! $review['title'] !!} </a>
                             @else
                                 <span class="author">{{ $review['author'] }}</span>
                             @endif
@@ -144,6 +146,69 @@
     @endauth
 
 
+
+
+{{--    <!-- Modal -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>--}}
+{{--    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+        <form class="form modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title ml-3">Оставить отзыв</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body pr-5 mt-4">
+                <div class="form-group row mb-4 pb-1">
+                    <label class="col-sm-3 col-form-label text-left text-sm-right">Достоинства <span class="text-danger">*</span></label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" placeholder="Достоинства" name="good">
+                    </div>
+                </div>
+                <div class="form-group row mb-4 pb-1">
+                    <label class="col-sm-3 col-form-label text-left text-sm-right">Недостатки <span class="text-danger">*</span></label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" placeholder="Недостатки" name="bad">
+                    </div>
+                </div>
+                <div class="form-group row mb-4 pb-1">
+                    <label class="col-sm-3 col-form-label text-left text-sm-right">Комментарий</label>
+                    <div class="col-sm-9">
+                        <textarea class="form-control" name="comment" rows="7" style="margin-top: 0px; margin-bottom: 0px; height: 254px;"></textarea>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer d-block text-center text-sm-left d-sm-flex justify-content-end pt-2 mb-2 px-5">
+                <div id="colorstar" data-rate="1" class="starrr ratable text-center mr-3 d-block d-sm-inline-block mb-3 mb-sm-0">
+                    <span data-num="1" class="fas fa-star"></span>
+                    <span data-num="2" class="far fa-star"></span>
+                    <span data-num="3" class="far fa-star"></span>
+                    <span data-num="4" class="far fa-star"></span>
+                    <span data-num="5" class="far fa-star"></span>
+                </div>
+                <button type="button" class="btn btn-primary px-5 send-review">Отправить отзыв</button>
+            </div>
+        </form>
+    </div>--}}
+
     <div id="noty_layout__bottomLeft" role="alert" aria-live="polite" class="noty_layout animate__animated animate__fadeInRightBig animate__faster" style="display: none">
         <div id="noty_bar_9da52369-9ae9-49da-858f-5f3687604672"
              class="noty_bar noty_type__error noty_theme__nest noty_close_with_click noty_has_timeout noty_has_progressbar">
@@ -151,6 +216,9 @@
             <div class="noty_progressbar" style="transition: width 4000ms linear 0s; width: 0%;"></div>
         </div>
     </div>
+
+
+
 
 
 @endsection
