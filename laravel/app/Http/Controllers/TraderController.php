9a153c209ee0b9540be5b0fcfcd25d2b25197c6b
skripniks044@gmail.com <skripniks044@gmail.com>
2020-10-25 16:29:39 +0200

    delete code string

--- laravel/app/Http/Controllers/TraderController.php
+++ laravel/app/Http/Controllers/TraderController.php
@@ -163,39 +163,6 @@ class TraderController extends Controller
             return $necessaryData;
         }
 
-        $rubrics = $this->traderService->getRubricsGroup();
-        $regions = $this->baseServices->getRegions();
-        $ports = $this->traderService->getPorts();
-        $currencies = $this->traderService->getCurrencies();
-
-        $traders = $this->traderService->getTradersRegionPortCulture(['port' => null, 'culture' => null, 'region' => $region, 'query' => $request->all()]);
-        $groups = $this->companyService->getRubricsGroup();
-
-        //$this->baseService->setBreadcrumbs(['region' => $data_region['region'], 'page_type' => 1]);
-
-
-        $data = ['rubric' => null, 'region' => $region, 'port' => null, 'type' => 0, 'page' => 1, 'onlyPorts' => null];
-
-        $meta = $this->seoService->getTradersMeta($data);
-
-        return view('traders.traders_regions',
-            [
-                'viewmod' => $request->get('viewmod'),
-                'regions' => $regions,
-                'traders' => $traders,
-                'rubric' => $rubrics,
-                'onlyPorts' => $ports,
-                'currencies' => $currencies,
-                'meta' => $meta,
-                'region_name' => $data_region['region'],
-                'current_region' => $region,
-                'rubricGroups' => $rubrics,
-                'group_culture_id' => null,
-                'isMobile' => $this->agent->isMobile(),
-                'page_type' => 1,
-                'breadcrumbs' => $meta['h1']
-            ]
-        );
     }
 
     /**
@@ -219,51 +186,6 @@ class TraderController extends Controller
         if($necessaryData){
             return $necessaryData;
         }
-
-
-        $rubrics = $this->traderService->getRubricsGroup();
-        $regions = $this->baseServices->getRegions();
-        $ports = $this->traderService->getPorts();
-        $currencies = $this->traderService->getCurrencies();
-        $traders = $this->traderService->getTradersRegionPortCulture(['port' => null, 'culture' => $culture, 'region' => $region, 'query' => $request->all()]);
-
-        $region_name = $this->traderService->getNamePortRegion($region);
-
-        $culture_name = TradersProducts::where('url', $culture)->value('id');
-        $culture_name = Traders_Products_Lang::where('item_id', $culture_name)->get()->toArray()[0];
-
-
-        $group_culture_id = TradersProducts::where('id', $culture_name['item_id'])->value('group_id');
-        $group_culture_id = TradersProductGroupLanguage::where('id', $group_culture_id)->value('id');
-
-        $data = [
-            'rubric' => $culture_name['name'], 'region' => $region, 'port' => null, 'type' => 0, 'page' => 1,
-            'onlyPorts' => null
-        ];
-
-        //$bread = $this->baseService->setBreadcrumbs(['region' => $region, 'rubric' => $culture, 'page_type' => 1]);
-
-        $meta = $this->seoService->getTradersMeta($data);
-
-
-        return view('traders.traders_regions_culture', [
-            'viewmod' => $request->get('viewmod'),
-            'regions' => $regions,
-            'traders' => $traders,
-            'rubric' => $rubrics,
-            'onlyPorts' => $ports,
-            'currencies' => $currencies,
-            'region_name' => $region_name['region'],
-            'current_region' => $region,
-            'current_culture' => $culture,
-            'culture_name' => $culture_name['name'],
-            'meta' => $meta,
-            'group_culture_id' => $group_culture_id,
-            'isMobile' => $this->agent->isMobile(),
-            'rubricGroups' => $rubrics,
-            'page_type' => 1
-        ]);
-
     }
 
     public function port(Request $request, $port)
@@ -280,37 +202,6 @@ class TraderController extends Controller
             return $necessaryData;
         }
 
-        $rubrics = $this->traderService->getRubricsGroup();
-        $regions = $this->baseServices->getRegions();
-        $ports = $this->traderService->getPorts();
-        $currencies = $this->traderService->getCurrencies();
-        $traders = $this->traderService->getTradersRegionPortCulture(['port' => $port, 'culture' => null, 'region' => null, 'query' => $request->all()]);
-
-        $data_port = $this->traderService->getNamePortRegion(null, $port);
-
-        $data = [
-            'rubric' => null, 'region' => null, 'port' => $data_port['port'], 'type' => 0, 'page' => 1,
-            'onlyPorts' => $data_port['onlyPorts']
-        ];
-
-        //$bread = $this->baseService->setBreadcrumbs(['port' => $port, 'page_type' => 1]);
-
-        $meta = $this->seoService->getTradersMeta($data);
-
-        return view('traders.traders_port', [
-            'viewmod' => $request->get('viewmod'),
-            'regions' => $regions,
-            'traders' => $traders,
-            'rubric' => $rubrics,
-            'onlyPorts' => $ports,
-            'currencies' => $currencies,
-            'meta' => $meta,
-            'port_name' => $data_port['port'],
-            'isMobile' => $this->agent->isMobile(),
-            'current_port' => $port,
-            'rubricGroups' => $rubrics,
-            'page_type' => 1
-        ]);
     }
 
 
@@ -327,46 +218,6 @@ class TraderController extends Controller
         if($necessaryData){
             return $necessaryData;
         }
-
-        $rubrics = $this->traderService->getRubricsGroup();
-        $regions = $this->baseServices->getRegions();
-        $ports = $this->traderService->getPorts();
-        $currencies = $this->traderService->getCurrencies();
-        $traders = $this->traderService->getTradersRegionPortCulture(['port' => $port, 'culture' => $culture, 'region' => null, 'query' => $request->all()]);
-
-        $data_port = $this->traderService->getNamePortRegion(null, $port);
-
-
-        $culture_name = TradersProducts::where('url', $culture)->value('id');
-        $culture_name = Traders_Products_Lang::where('item_id', $culture_name)->value('name');
-
-        $data = [
-            'rubric' => $culture_name, 'region' => null, 'port' => $data_port['port'], 'type' => 0, 'page' => 1,
-            'onlyPorts' => $data_port['onlyPorts']
-        ];
-
-        $meta = $this->seoService->getTradersMeta($data);
-
-        //$data_traders = ['rubric' => $culture, 'port' => $port, 'page_type' => 1];
-        //$bread = $this->baseService->breadcrumbs($data_traders);
-
-        return view('traders.traders_port_culture', [
-            'viewmod' => $request->get('viewmod'),
-            'regions' => $regions,
-            'traders' => $traders,
-            'rubric' => $rubrics,
-            'onlyPorts' => $ports,
-            'currencies' => $currencies,
-            'port_name' => $data_port['port'],
-            'current_port' => $port,
-            'current_culture' => $culture,
-            'culture_name' => $culture_name,
-            'isMobile' => $this->agent->isMobile(),
-            'meta' => $meta,
-            'rubricGroups' => $rubrics,
-            'page_type' => 1
-        ]);
-
     }
 
     public function forwards()
