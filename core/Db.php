ee99d78c23a77c355fe78dd39916cbc2eba87ec8
Dmitry <djigirdds@gmail.com>
2020-10-28 19:07:24 +0200

    reviews

--- core/Db.php
+++ core/Db.php
@@ -17,11 +17,8 @@ class Db {
   public static $host = 'localhost';
   public static $db_name = 'agrotender';
   public static $user = 'root';
-<<<<<<< HEAD
   public static $password = 'root';
-=======
-  public static $password = '';
->>>>>>> 8d088682a5b7543bc3feba35582e300c4876264c
+
 
   public function __construct() {
     // building data source name from config
