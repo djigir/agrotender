2149f261b7f4443abd4e18d1115adfa8839ac9a5
Alex <skripniks044@gmail.com>
2020-11-05 19:09:59 +0200

    fix bag

--- laravel/resources/views/filters/filter-companies.blade.php
+++ laravel/resources/views/filters/filter-companies.blade.php
@@ -80,12 +80,14 @@
                                     @if($rubric_id and $region)
                                         <a class="regionLink {{(!empty($obj_region) && $obj_region['translit'] == $region['translit']) ? 'active' : '' }}"
                                            href="{{route('company.region_culture', [$region['translit'], $rubric_id])}}">
-                                            <span>{{$region['name'] != 'Вся Украина' ? $region['name'].' область' : 'Вся Украина'}} </span>
+                                            <span>{{$region['name'] != 'Вся Украина' ? $region['name'].' область' : 'Вся Украина'}}</span>
+                                            <span class="companyCount small">({{$region['count_items']}})</span>
                                         </a>
                                     @else
                                         <a class="regionLink {{(!empty($obj_region) && $obj_region['translit'] == $region['translit']) ? 'active' : ''}}"
                                            href="{{route('company.region', $region['translit'])}}">
-                                            <span>{{$region['name'] != 'Вся Украина' ? $region['name'].' область' : 'Вся Украина'}} </span>
+                                            <span>{{$region['name'] != 'Вся Украина' ? $region['name'].' область' : 'Вся Украина'}}</span>
+                                            <span class="companyCount small">({{$region['count_items']}})</span>
                                         </a>
                                     @endif
                                 @endforeach
