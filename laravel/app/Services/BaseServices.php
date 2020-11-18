9c563184af32914feedc0f5342868c434f9f8a0e
skripniks044@gmail.com <skripniks044@gmail.com>
2020-11-18 11:05:07 +0200

    get price forward

--- laravel/app/Services/BaseServices.php
+++ laravel/app/Services/BaseServices.php
@@ -5,6 +5,7 @@ namespace App\Services;
 
 
 use App\Models\Regions\Regions;
+use Carbon\Carbon;
 
 class BaseServices
 {
@@ -35,113 +36,20 @@ class BaseServices
 
     }
 
-    /*public function setBreadcrumbsTraders($data)
+    public function getForwardsMonths()
     {
-        $breadcrumbs_trad[0] = ['name' => !empty($data['region_translit']) ? 'Цены трейдеров в Украине' : 'Цена на Аграрную продукцию в портах Украины', 'url' => null];
+        $data = [
+            'start' =>[],
+            'end' =>[],
+        ];
 
-        if($data['region_translit'] != 'ukraine' && !empty($data['region'])){
-            $breadcrumbs_trad[0] = ['name' => 'Цены трейдеров'. '<i style="margin-left: .5rem" class="fas fa-chevron-right extra-small"></i>', 'url' => route('traders.region', 'ukraine')];
-            $breadcrumbs_trad[1] = ['name' => "Цена Аграрной продукции в {$data['region']['parental']} области", 'url' => null];
+        for ($i = 0; $i <= 6; $i++){
+            $dt_start = Carbon::now();
+            $dt_end = Carbon::now();
+            array_push($data['start'], $dt_start->addMonths($i)->startOfMonth()->format('Y-m-d'));
+            array_push($data['end'],  $dt_end->addMonths($i)->endOfMonth()->format('Y-m-d'));
         }
 
-        if(($data['region_translit'] == 'ukraine' || $data['port_translit'] == 'all') && !empty($data['culture_name']))
-        {
-            $breadcrumbs_trad[0] = ['name' => 'Цены трейдеров'. '<i style="margin-left: .5rem" class="fas fa-chevron-right extra-small"></i>',
-                'url' => !empty($data['region_translit']) ? route('traders.region', $data['region_translit']) :
-                route('traders.port', $data['port_translit'])];
-            $breadcrumbs_trad[1] = ['name' => !empty($data['region_translit']) ?
-                "Закупочная цена {$data['culture_name']} на сегодня в Украине" : "Цена на {$data['culture_name']} в портах Украины", 'url' => null];
-        }
-
-        if (($data['region'] || $data['port']) && $data['culture_id'] && $data['region_translit'] != 'ukraine' && $data['port_translit'] != 'all'){
-            $breadcrumbs_trad[0] = ['name' => "Цены трейдеров" . '<i style="margin-left: .5rem" class="fas fa-chevron-right extra-small"></i>', 'url' =>
-                !empty($data['region_translit']) ? route('traders.region', 'ukraine') :
-                    route('traders.port', 'all')];
-
-            $breadcrumbs_trad[1] = ['name' => "Цена {$data['culture_name']}". '<i style="margin-left: .5rem" class="fas fa-chevron-right extra-small"></i>', 'url' =>
-                !empty($data['region_translit']) ? route('traders.region_culture', ['ukraine', $data['culture']]) :
-                route('traders.port_culture', ['all', $data['culture']])];
-
-            $breadcrumbs_trad[2] = ['name' =>
-                !empty($data['region_translit']) ? "Оптовые цены трейдеров на {$data['culture_name']} в {$data['region']['city_adverb']}" :
-                "Цена на {$data['culture_name']} в {$data['port']['portname']}", 'url' => null];
-        }
-
-        return $breadcrumbs_trad;
+        return $data;
     }
-
-
-    public function setBreadcrumbsTradersForward($data)
-    {
-        $breadcrumbs_trad_forward[0] = ['name' =>  "Форварды", 'url' => null];
-
-        if($data['port_translit'] != null && $data['port_translit'] == 'all'){
-            $breadcrumbs_trad_forward[0] = ['name' =>  "Форварды".'<i style="margin-left: .5rem" class="fas fa-chevron-right extra-small"></i>', 'url' => null];
-            $breadcrumbs_trad_forward[1] = ['name' => empty($data['culture_id']) ? "Форвардная цена на аграрную продукцию в портах Украины"
-                : "Форвардная цена на {$data['culture_name']} в портах Украины", 'url' => null];
-        }
-
-        if($data['region'] != null && $data['region_translit'] == 'ukraine'){
-            $breadcrumbs_trad_forward[0] = ['name' =>  "Форварды".'<i style="margin-left: .5rem" class="fas fa-chevron-right extra-small"></i>', 'url' => null];
-            $breadcrumbs_trad_forward[1] = ['name' => empty($data['culture_id']) ? "Форвардная цена на аграрную продукцию" : "Форвардная цена на {$data['culture_id']} в Украине", 'url' => null];
-        }
-
-
-        if($data['region'] && $data['culture_id'] && $data['region_translit'] != 'ukraine')
-        {
-            $breadcrumbs_trad_forward[0] = ['name' =>  "Форварды".'<i style="margin-left: .5rem" class="fas fa-chevron-right extra-small"></i>', 'url' => null];
-            $breadcrumbs_trad_forward[1] = ['name' => "Форварды {$data['culture_name']}".'<i style="margin-left: .5rem" class="fas fa-chevron-right extra-small"></i>' , 'url' => route('traders_forward.region_culture',['ukraine', $data['culture']])];
-            $breadcrumbs_trad_forward[2] = ['name' => "Форвардная цена на {$data['culture_name']} в {$data['region']['parental']} области" , 'url' => null];
-        }
-
-        if($data['port'] && $data['culture_id'] && $data['port_translit'] != 'all')
-        {
-            $breadcrumbs_trad_forward[0] = ['name' =>  "Форварды".'<i style="margin-left: .5rem" class="fas fa-chevron-right extra-small"></i>', 'url' => null];
-            $breadcrumbs_trad_forward[1] = ['name' => "Форварды {$data['culture_name']}".'<i style="margin-left: .5rem" class="fas fa-chevron-right extra-small"></i>' , 'url' => route('traders_forward.port_culture',['all', $data['culture']])];
-            $breadcrumbs_trad_forward[2] = ['name' => "Форвардная цена на {$data['culture_name']} в {$data['port']['portname']}" , 'url' => null];
-        }
-
-
-        return $breadcrumbs_trad_forward;
-    }
-
-
-    public function setBreadcrumbsTradersSell($data)
-    {
-        $breadcrumbs_trad_sell[0] = ['name' => "Продажи трейдеров", 'url' => null];
-
-        if($data['region_translit'] != 'ukraine' && $data['region']){
-            $breadcrumbs_trad_sell[0] = ['name' => "Продажи трейдеров". '<i style="margin-left: .5rem" class="fas fa-chevron-right extra-small"></i>', 'url' => route('traders_sell.region', 'ukraine')];
-            $breadcrumbs_trad_sell[1] = ['name' => "Аграрная продукция: предложения от трейдеров и переработчиков в {$data['region']['parental']} области", 'url' => null];
-        }
-
-        if($data['region_translit'] != 'ukraine' && $data['region'] && $data['culture_id'])
-        {
-            $breadcrumbs_trad_sell[0] = ['name' => "Продажи трейдеров". '<i style="margin-left: .5rem" class="fas fa-chevron-right extra-small"></i>', 'url' => route('traders_sell.region', 'ukraine')];
-            $breadcrumbs_trad_sell[1] = ['name' => "{$data['culture_name']}: предложения от трейдеров и переработчиков в {$data['region']['parental']} области", 'url' => ''];
-        }
-
-        return $breadcrumbs_trad_sell;
-    }
-
-    public function setBreadcrumbsCompanies($data)
-    {
-        $breadcrumbs_comp[0] = ['name' => 'Комании в Украине', 'url' => null];
-
-        if($data['region'] != 'ukraine' && $data['region']){
-            $breadcrumbs_comp[0] = ['name' => "Комании в {$data['region']['parental']} области " , 'url' => null];
-        }
-
-        if($data['region'] == 'ukraine' && !empty($data['rubric_id'])) {
-            $breadcrumbs_comp[0] = ['name' => "Комании в Украине" . '<i style="margin-left: .5rem" class="fas fa-chevron-right extra-small"></i>', 'url' => route('company.region', $data['region'])];
-            $breadcrumbs_comp[1] = ['name' => "Каталог - {$data['culture_name']} хозяйства Украины", 'url' => null];
-        }
-
-        if ($data['region'] && $data['rubric_id'] && $data['region'] != 'ukraine'){
-            $breadcrumbs_comp[0] = ['name' => "Комании в {$data['region']['parental']} области " . '<i style="margin-left: .5rem" class="fas fa-chevron-right extra-small"></i>', 'url' => route('company.region', $data['region']['translit'])];
-            $breadcrumbs_comp[1] = ['name' => "Каталог - {$data['culture_name']} хозяйства {$data['region']['city_parental']} ", 'url' => null];
-        }
-
-        return $breadcrumbs_comp;
-    }*/
 }
