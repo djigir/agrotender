3495e6e88f18977939c5aaedc7141e766dd79bcf
Dmitry <djigirdds@gmail.com>
2020-11-20 11:04:00 +0200

    user profile(company comments)

--- laravel/app/Services/User/ProfileService.php
+++ laravel/app/Services/User/ProfileService.php
@@ -87,8 +87,23 @@ class ProfileService
         }
     }
 
-    public function getUserReviews($type)
+    public function getUserCompanyReviews($type)
     {
+        // reviews for user company
+        if ($type) {
+            $company_reviews = CompComment::with('comp_comment_lang')->where('item_id', $this->userHasCompany()->id)->get();
+            $reviews = collect();
+
+            foreach ($company_reviews as $key => $company_comment) {
+                $reviews->add(CompItems::select('id', 'title', 'logo_file')->where('id', $company_reviews[$key]->item_id)->get()[0]);
+                $company_reviews[$key]->comp_title = $reviews[$key]->title;
+                $company_reviews[$key]->comp_id = $reviews[$key]->id;
+                $company_reviews[$key]->comp_logo = $reviews[$key]->logo_file;
+            }
+            return $company_reviews;
+
+        }
+        // user reviews
         $company_comments = CompComment::with('comp_comment_lang')->where('author_id', \auth()->user()->user_id)->get();
         $company_names = collect();
         foreach ($company_comments as $key => $company_comment) {
