b7f174b1763a1ef3f5ae353afd5f611420742a9c
skripniks044@gmail.com <skripniks044@gmail.com>
2020-11-20 11:53:59 +0200

    rename DB

--- core/Db.php
+++ core/Db.php
@@ -17,7 +17,7 @@ class Db {
   public static $host = 'localhost';
   public static $db_name = 'agrotender';
   public static $user = 'root';
-  public static $password = 'rooooooot';
+  public static $password = 'root';
 
 
 
