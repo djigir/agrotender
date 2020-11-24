47ef8f95122c6c88db529c37a3c8a33fa115369b
skripniks044@gmail.com <skripniks044@gmail.com>
2020-11-24 11:59:36 +0200

    fix bag

--- laravel/resources/views/mobile/filters/mobile-filter-traders.blade.php
+++ laravel/resources/views/mobile/filters/mobile-filter-traders.blade.php
@@ -127,16 +127,16 @@
                     @if(isset($rubricGroups))
                         @foreach($rubricGroups as $group => $item)
                             @foreach($rubricGroups[$group]['groups']["products"] as $index => $item_culture)
-                                <span class="culture px-4 py-3 my-3 content-block d-flex justify-content-between"  group="{{$item['id']}}" rubric="{{$item_culture['url']}}">
-                                    <span style="color: #1e56b2">{{ $item_culture['traders_product_lang'][0]['name']}}
-                                        @if($item_culture['count_item'] > 0)
-                                            <span class="companyCount small">({{$item_culture['count_item']}})</span>
-                                        @endif
+                                @if($item_culture['count_item'] > 0)
+                                    <span class="culture px-4 py-3 my-3 content-block d-flex justify-content-between"  group="{{$item['id']}}" rubric="{{$item_culture['url']}}">
+                                        <span style="color: #1e56b2">{{ $item_culture['traders_product_lang'][0]['name']}}
+                                                <span class="companyCount small">({{$item_culture['count_item']}})</span>
+                                        </span>
+                                        <span style="pointer-events: none">
+                                            <i style="color: #1e56b2;" class="far fa-chevron-right"></i>
+                                        </span>
                                     </span>
-                                    <span style="pointer-events: none">
-                                        <i style="color: #1e56b2;" class="far fa-chevron-right"></i>
-                                    </span>
-                                </span>
+                                @endif
                             @endforeach
                         @endforeach
                     @endif
