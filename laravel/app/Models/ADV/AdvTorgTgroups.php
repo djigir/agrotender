896ded07e993da8680f611599168cf9df4022c63
skripniks044@gmail.com <skripniks044@gmail.com>
2020-12-02 13:28:32 +0200

    sitemap

---
+++ laravel/app/Models/ADV/AdvTorgTgroups.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models\ADV;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+
+/**
+ * @property integer $id
+ * @property integer $sect0lev_id
+ * @property string $title
+ * @property string $url
+ * @property integer $sort_num
+
+ * @property Carbon $add_date
+ * @property Carbon $mod_date
+ */
+class AdvTorgTgroups extends Model
+{
+    protected $table = 'adv_torg_tgroups';
+
+    protected $fillable = [
+        'id', 'sect0lev_id',
+        'add_date', 'mod_date',
+        'title', 'url', 'sort_num'
+    ];
+}
---
+++ laravel/app/Models/ADV/AdvTorgTopic.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models\ADV;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property integer $parent_id
+ * @property integer $sort_num
+ * @property integer $sort_incol
+ * @property integer $visible
+ * @property string $descr
+ * @property string $title
+ * @property integer $menu_group_id
+ * @property string $page_h1
+ * @property string $page_title
+ * @property string $page_keywords
+ * @property string $page_descr
+ * @property string $seo_title_buy
+ * @property string $seo_keyw_buy
+ * @property string $seo_descr_buy
+ * @property string $seo_h1_buy
+ * @property string $seo_text_buy
+ * @property string $seo_keyw_sell
+ * @property string $seo_descr_sell
+ * @property string $seo_title_sell
+ * @property string $seo_h1_sell
+ * @property string $seo_text_sell
+ *
+ * @property Carbon $add_date
+*/
+class AdvTorgTopic extends Model
+{
+    protected $table = 'adv_torg_topic';
+
+    protected $fillable = [
+        'id', 'parent_id', 'sort_num', 'sort_incol', 'visible',
+        'add_date', 'descr', 'title', 'menu_group_id', 'page_h1',
+        'page_title', 'page_keywords', 'page_descr', 'seo_title_buy',
+        'seo_keyw_buy', 'seo_descr_buy', 'seo_h1_buy', 'seo_text_buy',
+        'seo_keyw_sell', 'seo_descr_sell', 'seo_title_sell', 'seo_h1_sell',
+        'seo_text_sell'
+    ];
+}
