ccd34c1bd6f4683a743982510af1144d3b7d1203
Dmitry <djigirdds@gmail.com>
2021-01-14 17:12:40 +0200

    new sections

---
+++ laravel/app/Models/Comp/CompItemsActive.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Comp;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CompItemsActive extends CompItems
+{
+    //
+}
---
+++ laravel/app/Models/Comp/CompItemsTraders.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Comp;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CompItemsTraders extends CompItems
+{
+    //
+}
