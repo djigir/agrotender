6c5d75babaf58fa203c44537ad7e1cb14dc4074e
skripniks044@gmail.com <skripniks044@gmail.com>
2021-01-19 12:02:19 +0200

    add styles admin

--- laravel/resources/views/vendor/sleeping_owl/default/_layout/base.blade.php
+++ laravel/resources/views/vendor/sleeping_owl/default/_layout/base.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
 	{!! $template->renderMeta($title) !!}
-
+    <link rel="stylesheet" href="/app/assets/css_admin/admin.css">
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
